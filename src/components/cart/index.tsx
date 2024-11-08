1003ecd206d8753b996a88a3744f569020f53852
minhtan16399 <minhtan3718a@gmail.com>
2024-11-08 15:51:41 +0700

    add cart

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -3,7 +3,7 @@ import { Drawer } from 'antd';
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "../../redux/store";
 import {formatPrice} from "../../shared/helper/function";
-import {deleteCartAction} from "../../redux/Reducers/cart-reducer";
+import {decreaseAmountProduct, deleteCartAction, increaseAmountProduct} from "../../redux/Reducers/cart-reducer";
 
 type CartProps = {
     label: React.ReactNode | '',
@@ -24,11 +24,11 @@ export const Cart = ({label, className}: CartProps) => {
 
     return (
         <>
-            <button className={className} onClick={showDrawer} style={{'position': 'relative'}}>
+            <button className={`${className}`} onClick={showDrawer} style={{'position': 'relative'}}>
                 {label}
-                <span className={'bg-red-500 text-white absolute rounded-[50%] size-5 text-xs -right-3 -top-3 flex justify-center items-center'}>{cart.count}</span>
+                <span className={'bg-red-500 text-white absolute rounded-[50%] size-5 text-xs -right-1.5 -top-2.5 flex justify-center items-center'}>{cart.count}</span>
             </button>
-            <Drawer title="Cart" onClose={onClose} open={open}>
+            <Drawer title="Cart" onClose={onClose} open={open} className={'relative'}>
                 {cart.cart.map((item, index) => (
                     <div key={index} className={'grid grid-cols-3 border-b py-3 relative'}>
                         <div className={'col-span-1'}>
@@ -36,15 +36,24 @@ export const Cart = ({label, className}: CartProps) => {
                         </div>
                         <div className={'col-span-2'}>
                             <div className={'font-bold'}>{item.name}</div>
-                            <div>{item.version}</div>
-                            <div>{item.color}</div>
-                            <div>{item.amount}</div>
-                            <div>{formatPrice(item.price * item.amount)}</div>
+                            <div>Phiên bản: {item.version}</div>
+                            <div>Màu sắc: {item.color}</div>
+                            <div className={'flex gap-3 items-center'}>
+                                <span>Số lượng: </span>
+                                <button className={'hover:scale-150'} onClick={() => dispatch(decreaseAmountProduct(item))}>-</button>
+                                <span>{item.amount}</span>
+                                <button className={'hover:scale-150'} onClick={() => dispatch(increaseAmountProduct(item))}>+</button>
+                            </div>
+                            <div>Tổng giá: {formatPrice(item.price * item.amount)}</div>
                         </div>
                         <button className={'absolute right-0 bottom-4 px-3 py-1 hover:text-white rounded hover:bg-red-600 border'}
                                 onClick={()=> dispatch(deleteCartAction(item))}>x</button>
                     </div>
                 ))}
+                <div className={'fixed bottom-0 w-[330px] bg-white py-2 flex justify-between items-center'}>
+                    <span className={'font-semibold'}>Tổng cộng: <span className={'text-red-500'}>{formatPrice(cart.total)}</span></span>
+                    <button className={'px-4 py-3 rounded bg-amber-500 font-semibold text-white hover:bg-green-500'}>Thanh toán</button>
+                </div>
             </Drawer>
         </>
     );
--- src/redux/Reducers/cart-reducer/index.ts
+++ src/redux/Reducers/cart-reducer/index.ts
@@ -3,12 +3,14 @@ import {CartItem} from "../../../shared/types/list-products-type";
 
 export interface CartState {
     cart: CartItem[],
-    count: number
+    count: number,
+    total: number
 }
 
 const initialState: CartState = {
     cart: [],
-    count: 0
+    count: 0,
+    total: 0
 }
 
 export const cartSlice = createSlice({
@@ -22,9 +24,11 @@ export const cartSlice = createSlice({
             if (productItem) {
                 productItem.amount = productItem.amount + productInCart.amount;
                 state.count = state.count + productInCart.amount;
+                state.total = state.total + (productInCart.price * productInCart.amount);
             } else {
                 state.cart.push(productInCart)
                 state.count = state.count + productInCart.amount;
+                state.total = state.total + (productInCart.price * productInCart.amount);
             }
         },
 
@@ -32,10 +36,34 @@ export const cartSlice = createSlice({
             const product = action.payload;
             const productInCart = { ...product };
             state.cart = state.cart.filter(item => (item.id !== productInCart.id || item.color !== productInCart.color || item.version !== productInCart.version));
+            state.count = state.count - productInCart.amount;
+            state.total = state.total - (productInCart.price * productInCart.amount);
+        },
+
+        increaseAmountProduct: (state, action) => {
+            const product = action.payload;
+            const productInCart = { ...product };
+            const productItem = state.cart.find(item => item.id === productInCart.id && item.color === productInCart.color && item.version === productInCart.version);
+            if (productItem) {
+                productItem.amount = productItem.amount + 1;
+            }
+            state.count = state.count + 1;
+            state.total = state.total + productInCart.price;
+        },
+
+        decreaseAmountProduct: (state, action) => {
+            const product = action.payload;
+            const productInCart = { ...product };
+            const productItem = state.cart.find(item => item.id === productInCart.id && item.color === productInCart.color && item.version === productInCart.version);
+            if (productItem) {
+                productItem.amount = productItem.amount - 1;
+            }
+            state.count = state.count - 1;
+            state.total = state.total - productInCart.price;
         },
     },
 })
 
-export const { addCartAction, deleteCartAction } = cartSlice.actions
+export const { addCartAction, deleteCartAction, increaseAmountProduct, decreaseAmountProduct } = cartSlice.actions
 
 export default cartSlice.reducer
